1002e6cb001ed96448540625b31da03b2f206244
supraja <sxboppana4@shockers.wichita.edu>
2023-04-05 00:01:08 +0700

    Add: ExercisePage (Events)

--- CalculatorApp/src/Calculator/ExercisePage.xaml.cs
+++ CalculatorApp/src/Calculator/ExercisePage.xaml.cs
@@ -2,13 +2,42 @@ namespace Calculator;
 
 public partial class ExercisePage : ContentPage
 {
+    public string selectedResult = "";
 	public ExercisePage()
 	{
 		InitializeComponent();
+        InitComponents();
 	}
+
+    private void InitComponents()
+    {
+
+    }
 
     private void NextButton_Clicked(object sender, EventArgs e)
     {
+        if (selectedResult == "") return;
+        else
+        {
+            if (selectedResult == "20")
+            {
+                RightResult.IsVisible = true;
+                Problem.IsVisible = false;
+                WrongResult.IsVisible = false;
+                RightButton.IsVisible = true;
+                WrongButtons.IsVisible = false;
+                Thread.Sleep(100);
+
+            }
+            else
+            {
+                RightResult.IsVisible = false;
+                Problem.IsVisible = false;
+                WrongResult.IsVisible = true;
+                RightButton.IsVisible = false;
+                WrongButtons.IsVisible = true;
+            }
+        }
     }
 
     private void AgainButton_Clicked(object sender, EventArgs e)
@@ -19,5 +48,11 @@ public partial class ExercisePage : ContentPage
     private void SkipButton_Clicked(object sender, EventArgs e)
     {
 
+    }
+
+    private void PossibleResult_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        RadioButton button = sender as RadioButton;
+        selectedResult = button.Content.ToString();
     }
 }
