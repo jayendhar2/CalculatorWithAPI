61c30c5dff0ea3a8c1d0b66ad029b881964acbb5
jayendhar <g927b543@wichita.edu>
2023-04-06 12:40:56 +0700

    Add: API Response Model

---
+++ CalculatorApp/src/Calculator/Problem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Problem
+    {
+        public string problem { get; set; }
+        public int rightResult { get; set; }
+        public int wrongResult1 { get; set; }
+        public int wrongResult2 { get;set; }
+    }
+}
