5b30a4571efc4aad065abfe4d3f33826d005acbd
kranthi <ksguda@shockers.wichita.edu>
2023-04-06 12:32:39 +0700

    Fix: Https Certificate Exception

--- CalculatorApp/src/Calculator/Platforms/Android/MainActivity.cs
+++ CalculatorApp/src/Calculator/Platforms/Android/MainActivity.cs
@@ -1,10 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Javax.Net.Ssl;
+using Org.Apache.Http.Conn.Ssl;
 
 namespace Calculator;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    //To ignore https certificate
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+#if DEBUG
+        HttpsURLConnection.DefaultHostnameVerifier = new AllowAllHostnameVerifier();
+#endif
+        base.OnCreate(savedInstanceState);
+    }
 }
